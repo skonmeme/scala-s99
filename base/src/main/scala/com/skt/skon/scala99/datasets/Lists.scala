38e1b68163b49a66ec7f28371bee5eb4f946be69
Sung Gon Yi <skonmeme@gmail.com>
2019-03-26 17:51:39 +0900

    Add P21-P28

--- base/src/main/scala/com/skt/skon/scala99/datasets/Lists.scala
+++ base/src/main/scala/com/skt/skon/scala99/datasets/Lists.scala
@@ -9,5 +9,8 @@ object Lists {
   val p7 = List(List(1, 1), 2, List(3, List(5, 8)))
   val p8 = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
   val p14 = List('a, 'b, 'c, 'c, 'd)
+  val p16 = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
+  val p27 = List('a, 'b, 'c, 'd, 'e, 'f, 'g)
+  val p28 = List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o))
 
 }
