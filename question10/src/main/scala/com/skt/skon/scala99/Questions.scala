a32cc648ebbb048a037a4fb4e83ef1cf8a46eac9
Sung Gon Yi <skonmeme@gmail.com>
2019-03-26 10:24:23 +0900

    Add println

--- question10/src/main/scala/com/skt/skon/scala99/Questions.scala
+++ question10/src/main/scala/com/skt/skon/scala99/Questions.scala
@@ -13,12 +13,12 @@ object Questions {
     val a1 = Lists.p1.last
     // List.q1.head
     // List.q1.tail
-    //println(a1)
+    println("P1: " + a1)
 
     // P2: Find the last but one element of a list.
     val a2 = Lists.p1.reverse.apply(1)
     // Lists.q1.reverse.iterator
-    //println(a2)
+    println("P2: " + a2)
 
     // P3: Find the Kth element of a list.
     val k = 3
@@ -28,26 +28,28 @@ object Questions {
     //  case None => println("Out of range")
     //  case _ => println(a3)
     //}
-    //println(a3)
+    println("P3: " + a3)
 
     // P4: Find the number of elements of a list.
     val a4 = Lists.p1.length
+    println("P4: " + a4)
 
     // P5: Reverse a list.
     val a5 = Lists.p1.reverse
+    println("P5: " + a5)
 
     // P6: Find out whether a list is a palindrome.
     val a6 = (Lists.p6 == Lists.p6.reverse)
-    //println(a6)
+    println("P6: " + a6)
 
     // P7: Flatten a nested list structure.
     val a7 = WithTag.flatMap[Any](Lists.p7)
-    //println(a7)
+    println("P7: " + a7)
 
     // P8: Eliminate consecutive duplicates of list elements.
     val a8 = Lists.p8.aggregate(List[Symbol]())((acc, x) =>  if (acc == Nil || acc.last != x) acc :+ x else acc, _ ++ _)
     //val a8 = Lists.q8.distinct
-    //println(a8)
+    println("P8: " + a8)
 
     // P9: Pack consecutive duplicates of list elements into sublists.
     val a9 = Lists.p8.aggregate(List[List[Symbol]]())((acc, x) => {
@@ -56,7 +58,7 @@ object Questions {
         acc.dropRight(1) :+ (acc.last :+ x)
       else acc :+ List(x)
     }, _ ++ _)
-    //println(a9)
+    println("P9: " + a9)
 
     // P10: Run-length encoding of a list.
     val a10 = Lists.p8.aggregate(List[(Int, Symbol)]())((acc, x) => {
@@ -65,20 +67,20 @@ object Questions {
         acc.dropRight(1) :+ (acc.last._1 + 1, acc.last._2)
       else acc :+ (1, x)
     }, _ ++ _)
-    //println(a10)
+    println("P10: " + a10)
 
     // P11: Modified run-length encoding
     val a11 = a10.map {
       case (1, x: Symbol) => x
       case x => x
     }
-    //println(a11)
+    println("P11: " + a11)
 
     // P12: Decode a run-length encoded list.
     val a12 = a10.flatMap {
       case (n: Int, x) => List.fill(n)(x)
     }
-    //println(a12)
+    println("P12: " + a12)
 
     // P13: Run-length encoding of a list (direct solution).
     val a13 = Lists.p8.foldRight(List[(Int, Symbol)]())((x, acc) => {
@@ -86,15 +88,15 @@ object Questions {
       else if (acc.head._2 == x) (acc.head._1 + 1, x) +: acc.drop(1)
       else (1, x) +: acc
     })
-    //println(a13)
+    println("P13: " + a13)
 
     // P14: Duplicate the elements of a list.
     val a14 = Lists.p14.flatMap(List.fill(2)(_))
-    //println(a14)
+    println("P14: " + a14)
 
     // P15: Duplicate the elements of a list a given number of times.
     val a15 = Lists.p14.duplicate(k)
     //val a15 = WithOption.duplicate(Lists.p14, k)
-    //println(a15)
+    println("P15: " + a15)
   }
 }
