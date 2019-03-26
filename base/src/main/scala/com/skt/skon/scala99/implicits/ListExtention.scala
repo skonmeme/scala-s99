7717fe25ed1888a867746aec6c9cf0545ac122ba
Sung Gon Yi <skonmeme@gmail.com>
2019-03-26 17:51:39 +0900

    Add P16-P28

--- base/src/main/scala/com/skt/skon/scala99/implicits/ListExtention.scala
+++ base/src/main/scala/com/skt/skon/scala99/implicits/ListExtention.scala
@@ -12,5 +12,49 @@ class ListExtention[T](list: List[T]) {
     else Some(list.flatMap(List.fill(k)(_)))
   }
 
+  def unique: List[T] = {
+    def loop(set: Set[T], l: List[T]): List[T] = l match {
+      case head :: tail if set contains head => loop(set, tail)
+      case head :: tail => head :: loop(set + head, tail)
+      case Nil => Nil
+    }
+    loop(Set(), list)
+  }
+
+  private def rearrangementCombinations(n: Int, c: List[Int]): List[Int] = {
+    var length = n
+    var rc = c
+    var combination = List[Int]()
+
+    while (length > 0) {
+      if (rc == Nil || length - rc.head <= 0) {
+        combination :+= length
+        length = 0
+      } else {
+        combination :+= rc.head
+        length -= rc.head
+        rc = rc.drop(1)
+      }
+    }
+    combination
+  }
+
+  // Very inefficient
+  def combinationsWithReplacement(c: List[Int]): List[List[List[T]]] = {
+    val nc = if (c.sum != c.length) rearrangementCombinations(list.length, c) else c
+
+    val comb = list.permutations
+    val ecomb = comb.map(l => {
+      var nl = l
+      var newList = List[List[T]]()
+      for (i <- nc) {
+        newList :+= nl.take(i).sortBy(_.toString)
+        nl = nl.drop(i)
+      }
+      newList
+    })
+    ecomb.toList.distinct
+  }
+
 }
 
